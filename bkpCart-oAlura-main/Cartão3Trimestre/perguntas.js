bb2ebe97e997906f74bf18065cf2e5379c8404a2
cls4nt <142804556+cls4nt@users.noreply.github.com>
2024-10-22 11:02:36 -0300

    Add files via upload

---
+++ bkpCart-oAlura-main/Cartão3Trimestre/perguntas.js
@@ -0,0 +1,26 @@
+criaCartao(
+    // categoria,
+    'Programação'
+    // pergunta,
+    'O que é JavaScript?'
+    // resposta,
+    'O JavaScriot é uma linguagem de programação'
+)
+
+criaCartao(
+    // categoria,
+    'Programação'
+    // pergunta,
+    'O que é JavaScript?'
+    // resposta,
+    'O JavaScriot é uma linguagem de programação'
+)
+
+criaCartao(
+    // categoria,
+    'Programação'
+    // pergunta,
+    'O que é JavaScript?'
+    // resposta,
+    'O JavaScriot é uma linguagem de programação'
+)
